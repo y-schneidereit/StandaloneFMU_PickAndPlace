e7a507272f044e948f9329e5ad310fa021d2a7d2
Schneidereit Yannik <yannik.schneidereit@hoefliger.de>
2022-03-29 16:43:57 +0200

    SetupExperiment

--- src/simulator.c
+++ src/simulator.c
@@ -5,7 +5,7 @@
 
 // model specific constants
 #define GUID "{Altair-MotionView: 19926_PicknPlace2__MKS.fmu: 1646036875.8419533}"
-#define RESOURCE_LOCATION "file:///C:/Users/schyan01/github/StandaloneFMU_PickAndPlace" // absolut path to the unziped fmu
+#define RESOURCE_LOCATION "file:///C:/Users/schyan01/github/StandaloneFMU_PickAndPlace/PickAndPlace_2/resources" // absolut path to the unziped fmu
 
 // callback functions
 static void cb_logMessage(fmi2ComponentEnvironment componentEnvironment, fmi2String instanceName, fmi2Status status, fmi2String category, fmi2String message, ...) {
@@ -72,17 +72,18 @@ int main(int argc, char *argv[]) {
 	
 	fmi2Component c = InstantiatePtr("instance1", fmi2CoSimulation, GUID, RESOURCE_LOCATION, &callbacks, fmi2False, fmi2False);
 
-	/*if (!c) return 1;
+	if (!c) return 1;
 
 	fmi2Real Time = 0;
 	fmi2Real stepSize = 1;
-	fmi2Real stopTime = 10;
-
+	fmi2Real tolerance = 0.001;
+	fmi2Real stopTime = 9;
+	
 	// Informs the FMU to setup the experiment. Must be called after fmi2Instantiate and befor fmi2EnterInitializationMode
-	CHECK_STATUS(SetupExperimentPtr(c, fmi2False, 0, Time, fmi2False, 0));
-
+	CHECK_STATUS(SetupExperimentPtr(c, fmi2True, tolerance, Time, fmi2True, stopTime));
+	
 	// Informs the FMU to enter Initialization Mode.
-	CHECK_STATUS(EnterInitializationModePtr(c));
+	//CHECK_STATUS(EnterInitializationModePtr(c));
 
 	/*
 	fmi2ValueReference u_ref = 0;
@@ -96,40 +97,30 @@ int main(int argc, char *argv[]) {
 	CHECK_STATUS(fmi2SetBoolean(c, &u_ref, 1, &u));
 
 	CHECK_STATUS(fmi2ExitInitializationMode(c));
+	
+	printf("time, u, T\n");*/
 
-	printf("time, u, T\n");
+	//for (int nSteps = 0; nSteps <= 20; nSteps++) {
 
-	for (int nSteps = 0; nSteps <= 20; nSteps++) {
-
-		Time = nSteps * stepSize;
+		//Time = nSteps * stepSize;
 
 		// set an input
-		CHECK_STATUS(fmi2SetBoolean(c, &u_ref, 1, &u));
-
-		
+		//CHECK_STATUS(fmi2SetBoolean(c, &u_ref, 1, &u));
 
 		// perform a simulation step
-		CHECK_STATUS(fmi2DoStep(c, Time, stepSize, fmi2True));	//The computation of a time step is started.
+		CHECK_STATUS(DoStepPtr(c, Time, stepSize, fmi2True));	//The computation of a time step is started.
 		
 		// get an output
-		CHECK_STATUS(fmi2GetBoolean(c, &T_ref, 1, &T));
+		//CHECK_STATUS(fmi2GetBoolean(c, &T_ref, 1, &T));
 		
-		printf("%.2f, %d, %d\n", Time, u, T);
-
-		if (zaehler == 4)
-		{
-			u = !u;
-			zaehler = 0;
-		}
-
-		zaehler++;
-	}*/
+		//printf("%.2f, %d, %d\n", Time, u, T);
+	//}
 
 TERMINATE:
 
 	// clean up
 	if (status < fmi2Fatal) {
-		//freeInstancePtr(c);
+		FreeInstancePtr(c);
 	}
 	
 	FreeLibrary(libraryHandle);
